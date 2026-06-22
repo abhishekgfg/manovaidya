73ef941ac180da79a593838dba3ac5a3f2a69846
Abhishek Kumar <manovaidya2@gmail.com>
2026-06-22 14:25:11 +0530

    feat: implement ChildHealthCarePage with sections for concerns, signs, and support services

--- frontend/src/pages/ChildHealthCarePage.jsx
+++ frontend/src/pages/ChildHealthCarePage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ChildHealthCareHeroSection from "../childhealthcare/ChildHealthCareHeroSection";
+import ChildHealthCareConcernsSection from "../childhealthcare/ChildHealthCareConcernsSection";
+import ChildHealthCareSignsSection from "../childhealthcare/ChildHealthCareSignsSection";
+import ChildHealthCareSupportSection from "../childhealthcare/ChildHealthCareSupportSection";
+import ChildHealthCareTrustSection from "../childhealthcare/ChildHealthCareTrustSection";
 
 function ChildHealthCarePage() {
   return (
     <React.Fragment>
       <main>
         <ChildHealthCareHeroSection />
+        <ChildHealthCareConcernsSection />
+        <ChildHealthCareSignsSection />
+        <ChildHealthCareSupportSection />
+        <ChildHealthCareTrustSection />
       </main>
     </React.Fragment>
   );
